ccf47b06186b37429d3120192391abaf523d51bb
Shivam Rastogi <Shivam.Rastogi1@bp.com>
2023-08-18 07:02:23 +0100

    Added header

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 
-const AnotherHeading = (<h1>HI</h1>)
-
-const HeadingComponent =() => {
+const HeaderComponent =() => {
 
     return    (
-    <div id="container">
-        {AnotherHeading}
-        <h1>Hello world</h1>
-    </div>
+        <div className="header">
+            <div className='logo'>
+                <img src='https://png.pngtree.com/png-clipart/20201216/original/pngtree-grocery-logo-with-shopping-bag-and-food-png-image_5714803.jpg'/>
+            </div>
+            <div className='menu'>
+                <ul>
+                    <li>Home</li>
+                    <li>Offers</li>
+                    <li>Cart</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>    
+            </div>
+        </div>
 )}
 
+const SearchComponent = () =>{
 
+    return (
+        <div className='search'>
+            <div className='seach-box'>
+                <input type='text'/>
+            </div>
+        </div>
+    ) 
+}
+
+
+const AppComponent = ()=>{
+    return (
+        <>
+            <HeaderComponent/>
+            <SearchComponent/>
+        </>
+    )
+}
 const root= ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<HeadingComponent/>);
+root.render(<AppComponent/>);
