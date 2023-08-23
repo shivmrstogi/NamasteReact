0682340e39d1c0ed1e4b87637313368fa342b155
Shivam Rastogi <Shivam.Rastogi1@bp.com>
2023-08-23 06:30:37 +0100

    updated

--- App.js
+++ App.js
@@ -3,17 +3,6 @@ import  ReactDOM  from 'react-dom/client';
 import HeaderComponent from './src/components/Header';
 import RestrauntComponent from './src/components/Restraunt';
 
-
-// const SearchComponent = () =>{
-//     return (
-//         <div className='search'>
-//             <div className='search-box'>
-//                 <input type='text'/>
-//             </div>
-//         </div>
-//     ) 
-// }
-
 const AppComponent = ()=>{
     return (
         <>
--- src/components/Restraunt.js
+++ src/components/Restraunt.js
@@ -1,14 +1,30 @@
 import CardComponent from "./Card";
 import { restaurants } from "../utils/constants";
+import { useState } from "react";
 
 
 const RestrauntComponent = () =>{
+
+    const [restaurantsList,setrestaurantsList] = useState(restaurants);
+
+    function filterRestraunts(){
+        const filterdData = restaurantsList.filter((rest) => rest.info.avgRating>=4);
+        setrestaurantsList(filterdData);
+    }
+
     return (
+        <>
+        <div className='top-rated'>
+            <div className='top' onClick={filterRestraunts}>
+                Filter Top Rated Restaurant
+            </div>
+        </div>
         <div className='restraunts'>
             {
-                restaurants.map((item, index) => <CardComponent key={index} restraunt={item}/>)
+                restaurantsList.map((item, index) => <CardComponent key={index} restraunt={item}/>)
             }
         </div>
+        </>
     )
 }
 
